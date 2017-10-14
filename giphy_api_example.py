3a914e63fda1484a8a76f62371be5ae276b2c939
Unknown <gemadang@syr.edu>
2017-10-14 17:57:17 -0400

    giphy tested check

---
+++ giphy_api_example.py
@@ -0,0 +1,10 @@
+import urllib.request, json
+
+
+api_key = 'MXumeVsEgV4ncwe7aHDAdmwnNM4UkI92'
+concept = 'ryan gosling'.replace(' ', '+')
+
+data = json.loads(urllib.request.urlopen("http://api.giphy.com/v1/gifs/search?q={}&api_key={}&limit=5".format(concept,api_key)).read())
+print(json.dumps(data, sort_keys=True, indent=4))
+
+				
