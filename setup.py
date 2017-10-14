2743265ae17721a922803f18f105cb2a668ed2f1
Unknown <gemadang@syr.edu>
2017-10-14 18:01:44 -0400

    clarifai model json output

--- setup.py
+++ setup.py
@@ -6,4 +6,6 @@ app = ClarifaiApp(api_key='f789eaa60ab04869a2b80c752daa3484')
 model = app.models.get("general-v1.3")
 
 # predict with the model
-model.predict_by_url(url='https://samples.clarifai.com/metro-north.jpg')
+hello = model.predict_by_url(url='https://samples.clarifai.com/metro-north.jpg')
+
+print(json.dumps(hello, indent=1))
