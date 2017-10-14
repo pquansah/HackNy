3239fc77f953397c946f5f6a1abfbc192b4271ef
Unknown <gemadang@syr.edu>
2017-10-14 18:50:44 -0400

    parsing through json giphy data

--- giphy_api_example.py
+++ giphy_api_example.py
@@ -1,15 +1,18 @@
-import urllib
+import urllib.request
 import json
-
 
 api_key = 'MXumeVsEgV4ncwe7aHDAdmwnNM4UkI92'
 
-emotion_list = ['happy', 'sad', 'scared', 'angry', 'suprised', 'confused', 'excited', 'disgust']
-emo = emotion_list[4]
+emotion_list = ['happy', 'sad', 'scared', 'angry', 'surprised', 'confused', 'excited', 'disgust']
+emo = emotion_list[2]
 subject = input('Subject : ')
 concept = '{} {}'.format(emo, subject).replace(' ', '+')
 
 data = json.loads(urllib.request.urlopen("http://api.giphy.com/v1/gifs/search?q={}&api_key={}&limit=5".format(concept,api_key)).read())
-print(json.dumps(data, sort_keys=True, indent=4))
+embed_urls_list = []
+for gif in data['data']:
+    embed_urls_list.append(gif['embed_url'])
+    
+for url in embed_urls_list:
+    print(json.dumps(url))
 
-				
