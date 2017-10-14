b848ebdfc5f055b15609dc38c71c5169aebadd75
pquansah <pquansah@syr.edu>
2017-10-14 17:41:28 -0400

    test

---
+++ setup.py
@@ -0,0 +1,9 @@
+from clarifai.rest import ClarifaiApp
+
+app = ClarifaiApp(api_key='f789eaa60ab04869a2b80c752daa3484')
+
+# get the general model
+model = app.models.get("general-v1.3")
+
+# predict with the model
+model.predict_by_url(url='https://samples.clarifai.com/metro-north.jpg')
