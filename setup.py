01d306179f74118849328bfc68e62a4e7f1a96a7
pquansah <pquansah@syr.edu>
2017-10-15 00:51:21 -0400

    new changes

--- setup.py
+++ setup.py
@@ -1,13 +1,6 @@
 from clarifai.rest import ClarifaiApp
 from clarifai.rest import Image as ClImage
-from imgurpython import ImgurClient
 import json
-client = ImgurClient('3f50136dbb425b8', '15567e9f23b8d117b4ff3b2cfd5a5195f0def36d')
-
-# Example request
-items = client.gallery()
-for item in items:
-    print(item.link)
 
 client_id = '3f50136dbb425b8'
 client_secret = '15567e9f23b8d117b4ff3b2cfd5a5195f0def36d'
@@ -19,6 +12,7 @@ model = app.workflows.get('mood-flow')
 
 image = ClImage(url='https://sslg.ulximg.com/image/405x405/artist/1392767353_217f16228baaa5dc18c82925ac76edf6.jpg/9e89e114db44f266e044addd06e88d69/1392767353_kanye_west_wall_40.jpg')
 hello = model.predict([image])
+test = ['results'][0]
 # predict with the model
 
-##print(json.dumps(hello, indent=1))
+print(json.dumps(test, indent=1))
