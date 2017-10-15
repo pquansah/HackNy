84af74eaf49328d804c0e0df622150fa17f85005
Unknown <gemadang@syr.edu>
2017-10-14 20:20:22 -0400

    downgraded to python2 :(

--- giphy_api_example.py
+++ giphy_api_example.py
@@ -1,14 +1,14 @@
-import urllib.request
+import urllib
 import json
 
 api_key = 'MXumeVsEgV4ncwe7aHDAdmwnNM4UkI92'
 
 emotion_list = ['happy', 'sad', 'scared', 'angry', 'surprised', 'confused', 'excited', 'disgust']
 emo = emotion_list[2]
-subject = input('Subject : ')
-concept = '{} {}'.format(emo, subject).replace(' ', '+')
+subject = raw_input('Subject : ')
+concept = '{} {}'.format(subject, emo).replace(' ', '+')
 
-data = json.loads(urllib.request.urlopen("http://api.giphy.com/v1/gifs/search?q={}&api_key={}&limit=5".format(concept,api_key)).read())
+data = json.loads(urllib.urlopen("http://api.giphy.com/v1/gifs/search?q={}&api_key={}&limit=5".format(concept,api_key)).read())
 embed_urls_list = []
 for gif in data['data']:
     embed_urls_list.append(gif['embed_url'])
